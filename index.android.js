8e2a474601d0bd2cf00b0fa6eed4611aef8ac586
Li-Heng Liang <admin@lihengl.com>
2015-11-13 02:26:03 +0800

    Finish tutorial on the official documentation

--- index.android.js
+++ index.android.js
@@ -1,52 +1,89 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-
 var React = require('react-native');
+
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 var {
   AppRegistry,
+  Image,
+  ListView,
   StyleSheet,
   Text,
-  View,
+  View
 } = React;
 
 var Marketplace = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+  getInitialState: function () {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2
+      }),
+      loaded: false
+    };
+  },
+  componentDidMount: function () {
+    this.fetchData();
+  },
+  fetchData: function () {
+    fetch(REQUEST_URL).then((response) => response.json())
+    .then((responseData) => {
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+        loaded: true
+      });
+    }).done();
+  },
+  renderLoadingView: function () {
+    return (<View style={styles.container}>
+      <Text>{'Loading movies...'}</Text>
+    </View>);
+  },
+  renderMovie: function (movie) {
+    return (<View style={styles.container}>
+      <Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail} />
+      <View style={styles.rightContainer}>
+        <Text style={styles.title}>{movie.title}</Text>
+        <Text style={styles.year}>{movie.year}</Text>
       </View>
-    );
+    </View>);
+  },
+  render: function () {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+    return (<ListView
+      dataSource={this.state.dataSource}
+      renderRow={this.renderMovie}
+      style={styles.listView}
+    />);
   }
 });
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
-  welcome: {
+  listView: {
+    backgroundColor: '#F5FCFF'
+  },
+  rightContainer: {
+    flex: 1
+  },
+  title: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    marginBottom: 8,
+    textAlign: 'center'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  thumbnail: {
+    height: 81,
+    width: 53
   },
+  year: {
+    textAlign: 'center'
+  }
 });
 
 AppRegistry.registerComponent('Marketplace', () => Marketplace);
